6c903844e1f6bf0756e32f5dec4be265278c7fff
Jinli Duan <43664231+shirou10086@users.noreply.github.com>
2023-04-21 13:42:27 -0400

    Update json2mongo.py

--- json2mongo.py
+++ json2mongo.py
@@ -2,23 +2,21 @@ import pymongo
 import json
 
 def insert_json_to_mongodb(filename, collection):
-    # 读取JSON文件内容并转换为Python字典对象
+    # 先把json换成字典
     with open(filename, 'r', encoding='utf-8') as f:
         data_dict = json.load(f)
 
-    # 将字典插入集合
+    # 将字典插入collection
     collection.insert_one(data_dict)
 
 # 建立MongoDB连接
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-# 选择数据库
 db = client["bigdata"]
 
-# 选择集合
 collection = db["final"]
 
-# 插入多个JSON文件到MongoDB
+# 插入多个JSON文件到MongoDB调用上面函数
 insert_json_to_mongodb("data.json", collection)
 insert_json_to_mongodb("data_analyst.json", collection)
 insert_json_to_mongodb("data_scientist.json", collection)
