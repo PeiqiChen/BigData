9310f33efb54d5eb8241b768af0c0da1c3b9c015
Yuejia Yin <yuejia.yinn@gmail.com>
2023-04-22 20:55:58 -0400

    add jobs2mongp.py

--- json2mongo.py
+++ json2mongo.py
@@ -1,5 +1,6 @@
 import pymongo
 import json
+import os
 
 def insert_json_to_mongodb(filename, collection):
     # 先把json换成字典
@@ -17,6 +18,11 @@ db = client["bigdata"]
 collection = db["final"]
 
 # 插入多个JSON文件到MongoDB调用上面函数
-insert_json_to_mongodb("cloud_developer.json", collection)
-insert_json_to_mongodb("data_analyst.json", collection)
-insert_json_to_mongodb("data_scientist.json", collection)
+# 待插入的JSON文件所在目录
+json_dir = "data"
+
+# 遍历目录下的所有JSON文件，并插入到MongoDB中
+for filename in os.listdir(json_dir):
+    if filename.endswith(".json"):
+        filepath = os.path.join(json_dir, filename)
+        insert_json_to_mongodb(filepath, collection)
