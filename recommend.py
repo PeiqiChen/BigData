af29390b6b58a2dc21506981e4a3a3405091dadc
Jinli Duan <43664231+shirou10086@users.noreply.github.com>
2023-04-23 20:45:59 -0400

    Update recommend.py

--- recommend.py
+++ recommend.py
@@ -3,19 +3,45 @@ from sklearn.metrics.pairwise import cosine_similarity
 import json
 import numpy as np
 import pandas as pd
+from pymongo import MongoClient
+
 
 with open('transformer.json', 'r', encoding='utf-8') as f:
     # Load the contents of the file into a dictionary
     data = json.load(f)
 def recommend(userprofile, data):
-    # using bert
+    # 这个是keylist
     klist = list(data.keys())
+    #这个是valuelist，向量list
     embeddinglist = [list(map(float, data[k])) for k in klist]
+    #model搞一个
     model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
+    #encode一下user数据变成向量
     sentence_embedding = model.encode(userprofile)
+    #valuelist变成向量
     embeddinglist = [np.array(v, dtype=object) for v in embeddinglist]
+    #find similiarity
     similarity_matrix = cosine_similarity([sentence_embedding] , embeddinglist).flatten()
+    #panda去sort
     df=pd.DataFrame({"sentence":klist,"similarityscore":similarity_matrix})
-    df = df.sort_values(by=["similarityscore"], ascending=False).head(5)
-    return list(df["sentence"])
+    result_list = df.sort_values(by=["similarityscore"], ascending=False).head(10)["sentence"].tolist()
+
+    client = MongoClient('mongodb://localhost:27017')
+    db = client['bigdata']
+    ans=[]
+    for collection_name in db.list_collection_names():
+        collection = db[collection_name]
+        cursor = collection.find()
+
+        # Iterate through all documents in the collection
+        for document in cursor:
+            # Extract job description field
+            for jobid in document.keys():
+                if jobid.isdigit():
+                    job_description = document[jobid].get('job_description', None)
+                    job_title = document[jobid].get('job_title', None)
+                    job_apply_link = document[jobid].get('job_apply_link', None)
+                    if job_description in result_list:
+                        ans.append((job_title,job_apply_link))
+    return set(ans)
 print(recommend("researcher  new york", data))
