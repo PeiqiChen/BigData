3780a1a11b45de232340ba5678416be4571c5bcf
Yufan Guo <yg2742@nyu.edu>
2023-04-25 03:01:32 -0400

    add user and login backend

---
+++ client/src/component/recommendBody.js
@@ -0,0 +1,7 @@
+import "../App.css";
+import { useEffect, useState } from 'react'
+import JobCard from './JobCard.js';
+function recommendBody(){
+
+}
+export default recommendBody ;
--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from flask import Flask
 import json
+from flask_mongoengine import MongoEngine
+from flask_login import LoginManager
+from user import db
+from routes import bp as routes_bp
 
 app = Flask(__name__)
+app.register_blueprint(routes_bp)
 
 @app.route("/users")
 def users():
@@ -14,5 +19,6 @@ def search(role, location="United States"):
     print(role)
     print(location)
     return data
+
 if __name__ == "__main__":
     app.run(debug=True)
