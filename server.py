1f95b125b78736ae60cf2f41224c730b84a27325
Peiqi Chen <chenpeiqi9915@gmail.com>
2023-04-25 12:51:27 -0400

    fix front end

--- server.py
+++ server.py
@@ -1,16 +1,13 @@
 from flask import Flask
 import json
-<<<<<<< HEAD
-from search import find_jobs_in_location_and_title
-=======
-from flask_mongoengine import MongoEngine
-from flask_login import LoginManager
-from user import db
-from routes import bp as routes_bp
->>>>>>> 40f8326344d8a248eb60ada438ad042ced0221d3
+# from search import find_jobs_in_location_and_title
+# from flask_mongoengine import MongoEngine
+# from flask_login import LoginManager
+# from user import db
+# from routes import bp as routes_bp
 
 app = Flask(__name__)
-app.register_blueprint(routes_bp)
+# app.register_blueprint(routes_bp)
 
 @app.route("/users")
 def users():
@@ -18,12 +15,12 @@ def users():
 
 @app.route("/search/<role>/<location>")
 def search(role, location="United States"):
-    # f = open('test.json')
-    # data = json.load(f)
-    # print(role)
-    # print(location)
-    list = find_jobs_in_location_and_title(role, location)
-    data = json.dumps(list)
+    f = open('test.json')
+    data = json.load(f)
+    print(role)
+    print(location)
+    # list = find_jobs_in_location_and_title(role, location)
+    # data = json.dumps(list)
     return data
 
 if __name__ == "__main__":
