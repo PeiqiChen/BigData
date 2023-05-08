b83990a04f6a64dfec707218e4f3c2fd4df5a65b
Peiqi Chen <chenpeiqi9915@gmail.com>
2023-05-08 12:47:04 -0400

    merge server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, session, jsonify
 import json
 from search_filters import search_jobs
 from recommend import recommend
@@ -6,6 +6,9 @@ import pymongo
 import pandas as pd
 from bson.json_util import dumps
 import secrets
+from flask_login import LoginManager
+from user import User, users_collection, check_password_hash
+from bson.objectid import ObjectId
 
 
 
@@ -45,6 +48,7 @@ def recommend_job(username, userinfo):
     # data = json.load(f)
     # print(username, userinfo)
     ##
+    print("recommending......")
     document = db['users'].find_one(sort=[("_id", pymongo.DESCENDING)])
     tech_stack = document.get('tech_stack', '')
     location = document.get('location', '')
@@ -56,7 +60,7 @@ def recommend_job(username, userinfo):
     "Intrinsic", 1676937600, "FULLTIME",
     "Researcher", "Mountain View", "CA"], '''
     list = recommend(userinfo)
-    print(list)
+    # print(list)
     keys = ['job_title','job_publisher','job_id',
             'employer_name','job_posted_at_timestamp','job_employment_type',
             'job_job_title', 'job_city','job_state']
@@ -65,12 +69,12 @@ def recommend_job(username, userinfo):
     list = list.replace("[[","")
     list = list.replace("]]","")
     for l in list.split("], ["):
-        # print(l)
-        ele = l.split(", ")
+        print(l)
+        ele = l.split("\", \"")
+        # print(ele,",,,,,",ele[0])
         tmp = {}
         idx = 0
         for idx in range(0,len(ele)):
-            # print(ele)
             if ele[idx] == 'null':
                 tmp[keys[idx]] = ''
             else:
@@ -81,6 +85,62 @@ def recommend_job(username, userinfo):
     data = json.dumps(data_map)
     # print(data)
     return data
+
+
+@app.route('/register', methods= ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username =request.form.get('username')
+        password = request.form.get('password')
+        jobs_for_looking = request.form.get('jobs_for_looking')
+        tech_stack = request.form.get('tech_stack')
+        location = request.form.get('location')
+        user = User(username=username, password=password, jobs_for_looking=jobs_for_looking, tech_stack=tech_stack, location=location)
+        user.save()
+        return redirect('/success')
+
+    return render_template('index.html')
+
+@app.route('/success', methods=['GET', 'POST'])
+def success():
+
+
+    return render_template('success.html')
+
+ # Define route for user login
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        # Get form data from request
+        #data = request.get_json()
+        # Find the user in the database
+        user = users_collection.find_one({'username': request.form.get('username')})
+        print("1111" + user['password'])
+        if user and check_password_hash(user['password'], request.form.get('password')):
+        # If login credentials are valid, store user_id in session
+            session['id'] = str(user['_id'])
+            print(str(user['_id']))
+            return redirect('http://localhost:3000/react-octo-job-search-003')
+            #return jsonify({'success': True, 'username': user['username']})
+        else:
+            error_message = 'Invalid username or password. Please try again.'
+            return render_template('login.html', error_message=error_message)
+            #return jsonify({'success': False, 'message': error_message})
+    else:
+        return render_template('login.html')
+
+@app.route('/findall')
+def findall():
+    alluser =  users_collection.find()
+    for u in alluser:
+        print(u)
+    return render_template('findall.html',users=alluser)
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('id', None)
+    print("here")
+    return redirect('/login')
 
 
 if __name__ == "__main__":
