637216c90aeff5199f874cd0727a3d8c096bf926
Yuejia Yin <yuejia.yinn@gmail.com>
2023-05-11 20:12:27 -0400

    rename ETL and originjobs2mongo

--- originJobs2mongo.py
+++ originJobs2mongo.py
@@ -3,7 +3,7 @@ import json
 import os
 
 def insert_json_to_mongodb(filename, collection):
-    # 先把json换成字典
+    # convert json to diction
     with open(filename, 'r', encoding='utf-8') as f:
         data_dict = json.load(f)
 
@@ -14,16 +14,16 @@ def insert_json_to_mongodb(filename, collection):
         job_dict = data_dict["data"][i]
         collection.insert_one(job_dict)
 
-# 建立MongoDB连接
+# connect to mogodb
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 
 db = client["bigdata"]
 
-# 定义集合名称列表
+# All colletcions in db
 collections = ["cloud_developer", "data_scientist", "jobs_data", "product_manager", "researcher", "software_engineer", "technical_manager"]
 
-# 遍历列表并插入到MongoDB
+# insert to MongoDB
 for collection_name in collections:
     collection = db[collection_name]
     filename = f"data/{collection_name}.json"
